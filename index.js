9d8292b40c5cd4d43a1231f6219828d492bf2be7
anhqle1996 <anh.le@katalon.com>
2022-12-06 15:42:40 +0700

    set output for github action

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const core = require('@actions/core');
-const { default: axios } = require('axios');
+const github = require('@actions/github')
 const { promises: fs } = require('fs');
 const glob = require('glob')
 const path = require('path')
 const { XMLParser } = require('fast-xml-parser')
 
-
-// const ROOT_FOLDER = "C:\\Users\\anhqle\\Katalon Studio\\Sample Shopping cart"
-const ROOT_FOLDER = "C:\\Users\\anhqle\\Katalon Studio\\shopping-cart-tests"
+const ROOT_FOLDER = process.env.GITHUB_WORKSPACE;
 
 // const TEST_SUITE_SCRIPT_REGREX = "Test Suites/!(TestOps)**/*.groovy";
 const TEST_SUITE_PATTERN = "Test Suites/**/*.ts";
@@ -51,20 +49,25 @@ const processTSC = (tsc) => {
 }
 
 const main = async () => {
+  core.info('Start scanning the repository...');
   const testCases = await scanForEntity(TEST_CASE_PATTERN, '');
+  core.info(`Found ${testCases.length} test cases.`);
   const testSuites = await scanForEntity(TEST_SUITE_PATTERN, 'TestSuiteEntity');
+  core.info(`Found ${testSuites.length} test suites.`);
   const testSuiteCollections = await scanForEntity(TEST_SUITE_PATTERN, 'TestSuiteCollectionEntity', processTSC);
+  core.info(`Found ${testSuiteCollections.length} test suite collections.`);
   const profiles = await scanForEntity(EXECUTION_PROFILE_PATTERN, 'GlobalVariableEntities');
+  core.info(`Found ${profiles.length} profiles.`);
 
   const result = {
-    repositoryUrl: '', // get from github
+    repositoryUrl: github.context.repo.repo,
     branch: '',
     testSuites, 
     profiles, 
     testSuiteCollections, 
     testCases
   }
-  console.dir(result, { depth: null, colors: true })
+  core.setOutput('repository', result)
   //TODO: get signed URL from TestOps and upload result to S3
 }
 
