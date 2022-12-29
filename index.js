df72875fb9d5c47baabfb899b652b30c11b4b5ab
Anh Le <47583931+anhqle1996@users.noreply.github.com>
2022-12-29 21:45:35 +0700

    Export git repository to json  (#1)
    Generate sample repositories' metadata and upload to S3

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const core = require('@actions/core');
-const { default: axios } = require('axios');
+const github = require('@actions/github')
 const { promises: fs } = require('fs');
 const glob = require('glob')
 const path = require('path')
 const { XMLParser } = require('fast-xml-parser')
+const Services = require('./utils/Services.js');
 
-
-// const ROOT_FOLDER = "C:\\Users\\anhqle\\Katalon Studio\\Sample Shopping cart"
-const ROOT_FOLDER = "C:\\Users\\anhqle\\Katalon Studio\\shopping-cart-tests"
+const ROOT_FOLDER = core.getInput('path');
 
 // const TEST_SUITE_SCRIPT_REGREX = "Test Suites/!(TestOps)**/*.groovy";
 const TEST_SUITE_PATTERN = "Test Suites/**/*.ts";
@@ -15,7 +14,7 @@ const TEST_CASE_PATTERN = "Test Cases/**/*.tc";
 const EXECUTION_PROFILE_PATTERN = "Profiles/**/*.glbl"
 
 const parseOption = {
-  isArray: (name, jpath, isLeafNode, isAttribute) => name === 'testCaseLink' || name === 'TestSuiteRunConfiguration'
+  isArray: (name) => name === 'testCaseLink' || name === 'TestSuiteRunConfiguration'
 }
 const parser = new XMLParser(parseOption)
 
@@ -51,21 +50,38 @@ const processTSC = (tsc) => {
 }
 
 const main = async () => {
+  core.info('Start scanning the repository...');
   const testCases = await scanForEntity(TEST_CASE_PATTERN, '');
+  core.info(`Found ${testCases.length} test cases.`);
   const testSuites = await scanForEntity(TEST_SUITE_PATTERN, 'TestSuiteEntity');
+  core.info(`Found ${testSuites.length} test suites.`);
   const testSuiteCollections = await scanForEntity(TEST_SUITE_PATTERN, 'TestSuiteCollectionEntity', processTSC);
+  core.info(`Found ${testSuiteCollections.length} test suite collections.`);
   const profiles = await scanForEntity(EXECUTION_PROFILE_PATTERN, 'GlobalVariableEntities');
+  core.info(`Found ${profiles.length} profiles.`);
 
   const result = {
-    repositoryUrl: '', // get from github
+    repositoryUrl: github.context.repo.repo,
     branch: '',
     testSuites, 
     profiles, 
     testSuiteCollections, 
     testCases
   }
-  console.dir(result, { depth: null, colors: true })
-  //TODO: get signed URL from TestOps and upload result to S3
+
+  core.setOutput('repository', result);
+
+  const jsonFile = await fs.writeFile('repository.json', JSON.stringify(result))
+  .then(() => fs.readFile('repository.json', 'utf-8'));
+
+  const GITHUB_URL = core.getInput('github-url');
+
+  core.info('Getting signed URL...');
+  await Services.getS3PresignedUrl(GITHUB_URL).then((response) => {
+    const presignedUrl = response.data;
+    core.info('Start uploading...');
+    Services.putS3PresignedUrl(presignedUrl, jsonFile);
+  })
 }
 
 main().catch(err => core.setFailed(err.message))
---
+++ utils/Apis.js
@@ -0,0 +1,6 @@
+const Apis = {
+
+    s3PresignedUrl: '/api/v1/patches/sample-git-repo-signed-url',
+};
+
+module.exports = Apis;
