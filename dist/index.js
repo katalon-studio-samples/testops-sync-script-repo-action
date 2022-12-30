6179e227dedcbeaf59b1913d5615c9511562a028
hiepvuhoang <hiep.vu@katalon.com>
2022-12-30 18:42:21 +0700

    udpate formate

--- dist/index.js
+++ dist/index.js
@@ -18216,9 +18216,7 @@ const Services = {
     },
 
     putS3PresignedUrl: (presignedS3Url, jsonFile) => {
-        return axios.put(presignedS3Url, {
-            file: jsonFile,
-        }, {
+        return axios.put(presignedS3Url, jsonFile, {
             headers: {
                 'Content-Type': 'application/json'
             }
