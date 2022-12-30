415fd0dbcca8109a89c080bbf2f15a9b96fb44b3
hiepvuhoang <hiep.vu@katalon.com>
2022-12-30 17:23:03 +0700

    log

--- dist/index.js
+++ dist/index.js
@@ -22370,7 +22370,7 @@ const main = async () => {
 
   core.setOutput('repository', result);
 
-  core.info(`Found github full link ${JSON.stringify(github.context.repo)}`);
+  core.info(`Found github full link ${JSON.stringify(github.context.payload)}`);
 
   const jsonFile = await fs.writeFile('repository.json', JSON.stringify(result))
   .then(() => fs.readFile('repository.json', 'utf-8'));
